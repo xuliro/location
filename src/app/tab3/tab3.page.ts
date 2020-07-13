47cb4f6a6b4f507a66afa8070e168b47518327f3
Ulisses Rodrigues <ulisses0205@gmail.com>
2020-07-13 17:43:54 -0300

    Adicionando calculo de deslocamento e ajustes na formatação da hora

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -12,12 +12,12 @@ import { BackgroundMode } from '@ionic-native/background-mode/ngx';
 })
 
 export class Tab3Page {
-  positionsArray : Array<{desc:string, lat:number , lon:number, data: Date, duration: string, id: number}> = [];
-  trajetoArray   : Array<{desc:string, lat:number , lon:number, data: Date, duration: string, id: number}> = [];
+  positionsArray : Array<{desc:string, lat:number , lon:number, begin: Date, end: Date, id: number}> = [];
+  trajetoArray   : Array<{desc:string, lat:number , lon:number, begin: Date, end: Date, id: number}> = [];
   lat = 0;
   lon = 0;
   desc = "";
-  data = new Date;
+  begin = new Date;
   startTime = new Date;
   duration = '';
   minDistance = Number.MAX_SAFE_INTEGER;
@@ -50,13 +50,13 @@ export class Tab3Page {
   }
 
   setFixedPositions(){
-    this.positionsArray.push({desc: 'PORTARIA',            lat : -19.9085995, lon : -43.9212425, data: new Date, duration: '0:0:0', id: 0});
-    this.positionsArray.push({desc: 'IGREJA',              lat : -19.9094514, lon : -43.9211431, data: new Date, duration: '0:0:0', id: 1});
-    this.positionsArray.push({desc: 'PRACINHA',            lat : -19.9105974, lon : -43.9213554, data: new Date, duration: '0:0:0', id: 2});
-    this.positionsArray.push({desc: 'BARÃO X CAMPESTRE',   lat : -19.9104510, lon : -43.9223549, data: new Date, duration: '0:0:0', id: 3});
-    this.positionsArray.push({desc: 'BARÃO X STA BARBARA', lat : -19.9091935, lon : -43.9220772, data: new Date, duration: '0:0:0', id: 4});
-    this.positionsArray.push({desc: 'BARÃO X BICAS',       lat : -19.9085199, lon : -43.9218706, data: new Date, duration: '0:0:0', id: 5});
-    this.positionsArray.push({desc: 'STA MARTA X BICAS',   lat : -19.9083427, lon : -43.9211549, data: new Date, duration: '0:0:0', id: 6});
+    this.positionsArray.push({desc: 'PORTARIA',            lat : -19.9085995, lon : -43.9212425, begin: new Date, end: new Date, id: 0});
+    this.positionsArray.push({desc: 'IGREJA',              lat : -19.9094514, lon : -43.9211431, begin: new Date, end: new Date, id: 1});
+    this.positionsArray.push({desc: 'PRACINHA',            lat : -19.9105974, lon : -43.9213554, begin: new Date, end: new Date, id: 2});
+    this.positionsArray.push({desc: 'BARÃO X CAMPESTRE',   lat : -19.9104510, lon : -43.9223549, begin: new Date, end: new Date, id: 3});
+    this.positionsArray.push({desc: 'BARÃO X STA BARBARA', lat : -19.9091935, lon : -43.9220772, begin: new Date, end: new Date, id: 4});
+    this.positionsArray.push({desc: 'BARÃO X BICAS',       lat : -19.9085199, lon : -43.9218706, begin: new Date, end: new Date, id: 5});
+    this.positionsArray.push({desc: 'STA MARTA X BICAS',   lat : -19.9083427, lon : -43.9211549, begin: new Date, end: new Date, id: 6});
   }
 
   startInterval(time){
@@ -67,7 +67,7 @@ export class Tab3Page {
   }
 
   send(){
-    this.positionsArray.push({desc: this.desc, lat : this.lat , lon : this.lon, data: new Date, duration: '0:0:0', id: this.lastId});
+    this.positionsArray.push({desc: this.desc, lat : this.lat , lon : this.lon, begin: new Date, end: new Date, id: this.lastId});
     this.lastId++;
     this.desc = "";
   }
@@ -86,8 +86,8 @@ export class Tab3Page {
       //Lê as coordenadas
       this.lat = resp.coords.latitude  == null ? 0 : resp.coords.latitude;
       this.lon = resp.coords.longitude == null ? 0 : resp.coords.longitude;
-      //Le a data atual
-      this.data = new Date;      
+      //Le a begin atual
+      this.begin = new Date;      
       this.pos = undefined;
       this.minDistance = Number.MAX_SAFE_INTEGER;
 
@@ -108,23 +108,48 @@ export class Tab3Page {
             this.startTime = new Date;
           }
           //Adiciona o trajeto
-          this.trajetoArray.push({desc: this.pos.desc, lat: this.lat, lon: this.lon, data: new Date, duration: '0:0:0', id: this.pos.id});
+          this.trajetoArray.push({desc: this.pos.desc, lat: this.lat, lon: this.lon, begin: new Date, end: new Date, id: this.pos.id});
         }
         //Testa se o ponto atual é o mesmo
         else if (this.trajetoArray[this.trajetoArray.length-1].id === this.pos.id){
           //atualiza o tempo decorrido (Duração)
-          let time = new Date().getTime() - this.trajetoArray[this.trajetoArray.length-1].data.getTime();          
-          this.trajetoArray[this.trajetoArray.length-1].duration = Math.round((time/3600000)%60) + ':' + Math.round((time/60000)%60) + ':' + Math.round((time/1000)%60);
+          let time = new Date().getTime() - this.trajetoArray[this.trajetoArray.length-1].begin.getTime();          
+          this.trajetoArray[this.trajetoArray.length-1].end = new Date;
         }
       }
       //Atualiza o tempo total
       let time = new Date().getTime() - this.startTime.getTime();
-      this.duration = Math.round((time/3600000)%60) + ':' + Math.round((time/60000)%60) + ':' + Math.round((time/1000)%60);
-
+      this.duration = this.miliToTimeStr(time, false);
       this.changeDetector.detectChanges();
      }).catch((error) => {
        console.log('Error getting location', error);
      });
+  }
+
+  miliToTimeStr (mili: number, gmt: boolean){
+    let string = "";
+    //Calcula Horas
+    let x = Math.floor((mili/3600000)%24);
+    if (gmt){
+      x = x-3;
+    }//-3 por causa do GMT-3
+    if (x <= 9){
+      string += "0";
+    }
+    string += x + ':';
+    //Calcula Minutos
+    x = Math.floor((mili/60000)%60);
+    if (x <= 9){
+      string += "0";
+    }
+    string += x + ':';
+    //Calcula Segundos
+    x = Math.floor((mili/1000)%60);
+    if (x <= 9){
+      string += "0";
+    }
+    string += x;
+    return string;
   }
 
   degToRad(n)
